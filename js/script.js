70da1de9860492d3293f696a9e6d561d57c6a0af
idosonaweb <jvieiraluzpiva@gmail.com>
2024-04-07 20:32:52 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-window.onscroll = function() {
-    Header_bottom()
-};
-
-let border = `
-        border-bottom: 1px solid #434040;`
-
-function Header_bottom() {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        document.getElementById(".header").style = border ;
-    } else {
-        document.getElementById(".header").style = "";
-    }
+function openMenu() {
+    document.getElementById("navlist").classList.toggle("open");
 }
-
-// function openMenu() {
-//     document.getElementById("menu").classList.toggle("open");
-//     document.getElementById("body").classList.toggle("hidden");
-//     document.getElementById("header-menu").classList.toggle("fecha");
-//     document.getElementById("header-close").classList.toggle("abre");
-// }
