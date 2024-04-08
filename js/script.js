1333d1d485c02bb58a5bcbd91c3fbd071f225cc1
idosonaweb <jvieiraluzpiva@gmail.com>
2024-04-08 15:38:44 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// FUNÇÂO SCROLL
+
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("scrollTop", window.scrollY > 40)
+});
+
+// FUNÇÂO MENU
+
+const body = document.querySelector("body");
+
 function openMenu() {
     document.getElementById("navlist").classList.toggle("open");
+    body.classList.toggle ("hidden");
 }
